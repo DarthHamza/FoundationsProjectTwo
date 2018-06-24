769efaa8e38cd32f797549e3bbfd5c50e796a259
DarthHamza <hamzamakia@gmail.com>
2018-06-24 18:57:25 +0300

    completed solution for foundations project 2

--- actions.py
+++ actions.py
@@ -1,42 +1,52 @@
-# UTILS AND FUNCTIONALITY
 from data import stores
 from components import Cart
 
-site_name = ""  # Give your site a name
+site_name = "The Place With Stuff"
 
 def welcome():
     print("Welcome to %s\nFeel free to shop throughout the stores we have, and only checkout once!" % site_name)
 
 def print_stores():
-    """
-    prints the list of stores in a nice readable format.
-    """
-    # your code goes here!
+    for store in stores:
+        print(store)
 
 def get_store(store_name):
-    """
-    receives a name for a store, and returns the store object with that name.
-    """
-    # your code goes here!
+    for store in stores:
+        if store.name.lower() == store_name.lower():
+            return store
+    return False
 
 def pick_store():
-    """
-    prints list of stores and prompts user to pick a store.
-    """
-    # your code goes here!
+    for store in stores:
+        print (store.name)
+    selected_store = input('Pick a store by typing its name. Or type "checkout" to pay your bills and say your goodbyes. ')
+    store_obj = get_store(selected_store)
+    while not(selected_store == 'checkout') and store_obj==False:
+        selected_store = input('No store with that name. Please try again. ')
+        store_obj = get_store(selected_store)
+
+    if selected_store == "checkout":
+        return "checkout"
+    return store_obj
 
 def pick_products(cart, picked_store):
-    """
-    prints list of products and prompts user to add products to card.
-    """
-    # your code goes here!
+    picked_store.print_products()
+    selected_product = input('Select a product you would like to purchase. Type "back" when you\'re done. ')
+    while not selected_product == "back":
+        if selected_product.lower() in [product.name.lower() for product in picked_store.products]:
+            cart.add_to_cart(selected_product)
+
+        if not selected_product.lower() in [product.name.lower() for product in picked_store.products]:
+            selected_product = input('No product with that name. Please try again. ')
+        else:
+            selected_product = input('What else do you want? ')
+    pick_store()
+
 
 def shop():
-    """
-    The main shopping functionality
-    """
+    selected_store = pick_store()
     cart = Cart()
-    # your code goes here!
+    pick_products(cart, selected_store)
 
 def thank_you():
     print("Thank you for shopping with us at %s" % site_name)
